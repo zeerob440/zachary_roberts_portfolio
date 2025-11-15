f58952972709c86eb874cf6ceb9759e3fbe38a0a
zeerob440 <zeerob440@tutanota.com>
2025-11-15 17:43:52 -0500

    fixed: toggle.js dedication plate date

--- toggle.js
+++ toggle.js
@@ -1,12 +1,12 @@
 //toggle.js is intended to switch the ccs the browser applies according to user input//
-//Proudly Engineered by Zachary Roberts  NOV 2025//
+//Proudly Engineered by Zachary Roberts 14 NOV 2025/ /
 //"Hold on to your butts." Ray Arnold, Jurassic Park//
 
 const toggle =document.getElementById("styleToggle");
 
 
 toggle.addEventListener("click", () => { 
-    const current = localStorage.getItem("modern");
+    const current = localStorage.getItem("skin");
 
     if (current === "retro") {
         localStorage.setItem("skin", "modern");
