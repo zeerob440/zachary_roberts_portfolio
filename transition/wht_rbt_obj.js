3973e053280c78c6d56e37f3d216798092bb1d29
zeerob440 <zeerob440@tutanota.com>
2025-11-15 12:32:49 -0500

    feat: wht_rbt_obj.js runs, need to be refined.

--- transition/wht_rbt_obj.js
+++ transition/wht_rbt_obj.js
@@ -1,2 +1,9 @@
 //Proudly Engineered by Zachary Roberts, inspired by Dennis Nedry 12 NOV 2025//
 //"It's the kicker. wht_rbt.obj, whatever it is, it did it all." -Ray Arnold, Jurassic Park//
+
+
+
+let ydstmw = "YOU DIDN'T SAY THE MAGIC WORD!";
+for (let i = 0; i < 50; i++) {
+    document.write(ydstmw + "<br>");
+}
