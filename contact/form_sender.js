616dbc9ddcb73e821c320de1387847cf38316e79
zeerob440 <zeerob440@tutanota.com>
2025-11-28 07:15:32 -0500

    fixed: after researching formspree form submission logic, form_sender.js was converted to input_validator.js to validate input before allowing the form to be sent.

--- contact/form_sender.js
+++
@@ -1,6 +0,0 @@
-/*form_sender.js sends forms completed by recruiters
-to form backend service. */
-
-/*Proudly Engineererd by Zachary Roberts 28 NOV 2025
-"You got mail!" -yahoo circa 1996*/
-
---
+++ contact/input_validator.js
@@ -0,0 +1,11 @@
+/*
+input_validator.js validates forms before they are
+sent to a backend service.
+
+Proudly Engineererd by Zachary Roberts 28 NOV 2025.
+"I'm sorry Dave. I'm afraid I cannot do that." -HAL9000, 2001: A Space Odyssey
+*/
+
+const validator =document.getElementById("sendIt");
+
+validator.addEventListener("click", )
